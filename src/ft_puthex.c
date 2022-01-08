4c5222b22ef7ac1c2b9fe7f9b09544f2cf4fd13f
eestelle <yosiky@list.ru>
2022-01-08 03:54:00 +0300

    Bonus for X x

--- src/ft_puthex.c
+++ src/ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: eestelle <eestelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/31 21:31:24 by eestelle          #+#    #+#             */
-/*   Updated: 2022/01/07 19:08:41 by eestelle         ###   ########.fr       */
+/*   Updated: 2022/01/08 03:43:24 by eestelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ static int	ft_dectohex(char *buff, size_t l, unsigned int value, int flag)
 	else
 		hex = (char *)hex_lower;
 	len = 0;
+	if ((int)value < 0)
+		buff[0] = hex[15];
 	if (value == 0)
 	{
 		buff[l - 1] = '0';
@@ -51,31 +53,110 @@ static int	ft_dectohex(char *buff, size_t l, unsigned int value, int flag)
 	return (len);
 }
 
-int	ft_puthex_lower(va_list data, flag_t *flag)
+static int	ft_func_one(t_flag *flag, int f1, int f2)
+{
+	int	len;
+
+	len = 0;
+	if (!f1 && flag->minus_zero == '0' && flag->sharp)
+	{
+		if (f2 == 'f')
+			len += write(1, "0x", 2);
+		else
+			len += write(1, "OX", 2);
+	}
+	while (flag->widht > 0)
+	{
+		len++;
+		write(1, &(flag->minus_zero), 1);
+		flag->widht--;
+	}
+	if (!f1 && flag->minus_zero == ' ' && flag->sharp)
+	{
+		if (f2 == 'f')
+			len += write(1, "0x", 2);
+		else
+			len += write(1, "OX", 2);
+	}
+	return (len);
+}
+
+static int ft_max(int a, int b, int c)
+{
+	if (c)
+	{
+		if (a > b)
+			return (a);
+		return (b);
+	}
+	return (a);
+}
+
+static int	ft_check_flag(t_flag *flag, int count, int f1, char f2)
+{
+	int	len;
+
+	len = 0;
+	flag->widht -= 2 * flag->sharp;
+	flag->widht -= ft_max(count, flag->precision, flag->flag_p);
+	if (flag->minus_zero != '-')
+		len += ft_func_one(flag, f1, f2);
+	while (flag->precision > count)
+	{
+		flag->precision--;
+		len++;
+		write(1, "0", 1);
+	}
+	return (len);
+}
+
+int	ft_puthex_lower(va_list data, t_flag *flag)
 {
 	int		len;
+	int		count;
 	char	buff[12];
 
 	len = ft_dectohex(buff, 12, va_arg(data, int), 0);
-	if (flag->sharp && !(len == 1 && buff[11] == '0'))
-		return (write(1, "0x", 2) + write(1, buff + 12 - len, len));
+	count = ft_check_flag(flag, len, len == 1 && buff[11] == '0', buff[0]);
+	if (!(flag->flag_p && flag->precision == 0 && len == 1 && buff[11] == '0'))
+		write(1, buff + 12 - len, len);
 	else
-		return (write(1, buff + 12 - len, len));
+	{
+		flag->widht++;
+		len = 0;
+	}
+	while (flag->widht > 0)
+	{
+		len += write(1, " ", 1);
+		flag->widht--;
+	}
+	return (count + len);
 }
 
-int	ft_puthex_upper(va_list data, flag_t *flag)
+int	ft_puthex_upper(va_list data, t_flag *flag)
 {
 	int		len;
+	int     count;
 	char	buff[12];
 
 	len = ft_dectohex(buff, 12, va_arg(data, int), 1);
-	if (flag->sharp && !(len == 1 && buff[11] == '0'))
-		return (write(1, "0X", 2) + write(1, buff + 12 - len, len));
+	count = ft_check_flag(flag, len, len == 1 && buff[11] == '0', buff[0]);
+	if (!(flag->flag_p && flag->precision == 0 && len == 1 && buff[11] == '0'))
+		write(1, buff + 12 - len, len);
 	else
-		return (write(1, buff + 12 - len, len));
+	{
+		flag->widht++;
+		len = 0;
+	}
+	while (flag->widht > 0)
+	{
+		len += write(1, " ", 1);
+		flag->widht--;
+	}
+	return (count + len);
 }
 
-int	ft_putadd(va_list data, flag_t *flag)
+int	ft_putadd(va_list data, t_flag *flag)
 {
 	char	buff[20];
 	int		count;
@@ -86,10 +167,8 @@ int	ft_putadd(va_list data, flag_t *flag)
 	f = 0;
 	if (value == 0)
 		return (write(1, "(nil)", 5));
-	if (flag->plus)
-		f = write(1, "+", 1);
-	else if (flag->space)
-		f = write(1, " ", 1);
+	if (flag->plus_space)
+		write(1, &(flag->plus_space), 1);
 	write(1, "0x", 2);
 	count = ft_dectohex_p(buff, 17, value);
 	return (2 + write(1, buff + 17 - count, count) + f);
